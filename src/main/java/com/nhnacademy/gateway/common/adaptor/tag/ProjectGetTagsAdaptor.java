64d2269183510eac79c3431dd81003fe0142bf5f
김재혁 <rlawog@nate.com>
2025-04-27 21:40:13 +0900

    fix. projectDetailForm.html 수정

---
+++ src/main/java/com/nhnacademy/gateway/common/adaptor/tag/ProjectGetTagsAdaptor.java
@@ -0,0 +1,39 @@
+package com.nhnacademy.gateway.common.adaptor.tag;
+
+import com.nhnacademy.gateway.exception.ResponseDtoException;
+import com.nhnacademy.gateway.model.dto.tag.ResponseTagsDto;
+import com.nhnacademy.gateway.model.dto.tag.TagNameRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/*
+프로젝트에 등록된 Tag 가져오기
+ */
+@Slf4j
+@Component
+public class ProjectGetTagsAdapter {
+    private static final String PROJECT_API_URL = "http://localhost:7070/project/";
+
+    private final RestTemplate restTemplate;
+
+    public ProjectGetTagsAdapter(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public ResponseTagsDto sendAndGetProjectTags(TagNameRequest tagNameRequest) {
+        try{
+            String url = PROJECT_API_URL + projectId + "/tag";
+            log.info(url);
+            ResponseEntity<ResponseTagsDto> response = restTemplate.getForEntity(PROJECT_API_URL + projectId + "/tag", ResponseTagsDto.class);
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                throw new ResponseDtoException("Project에 등록된 Tag 리스트를 가져오지 못했습니다.");
+            }
+            return response.getBody();
+        }catch(Exception e) {
+            log.error("try-catch: {}", e, e);
+            throw new ResponseDtoException("Project에 등록된 Tag 리스트를 가져오던 중 에러 발생");
+        }
+    }
+}
