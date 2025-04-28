4fa4368e116ab18abfdb298c08361f3a0af2b3f7
chossa <sua6458@gmail.com>
2025-04-28 17:44:03 +0900

    for pull

--- src/main/java/com/nhnacademy/gateway/controller/project/ProjectDetailController.java
+++ src/main/java/com/nhnacademy/gateway/controller/project/ProjectDetailController.java
@@ -3,15 +3,13 @@ package com.nhnacademy.gateway.controller.project;
 import com.nhnacademy.gateway.common.adaptor.login.MemberStatusAdaptor;
 import com.nhnacademy.gateway.common.adaptor.project.ProjectPostProjectStateAdaptor;
 import com.nhnacademy.gateway.model.domain.Project;
-import com.nhnacademy.gateway.model.dto.ResponseDto;
-import com.nhnacademy.gateway.model.dto.ResponseProjectDto;
-import com.nhnacademy.gateway.model.request.member.MemberCudRequest;
+import com.nhnacademy.gateway.model.dto.task.ResponseTaskDetailDto;
 import com.nhnacademy.gateway.model.request.project.ProjectIdRequest;
 import com.nhnacademy.gateway.model.request.project.ProjectMember;
 import com.nhnacademy.gateway.model.request.project.ProjectStateRequest;
-import com.nhnacademy.gateway.model.type.Cud;
 import com.nhnacademy.gateway.model.type.State;
 import com.nhnacademy.gateway.service.ProjectService;
+import com.nhnacademy.gateway.service.TaskService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,10 +37,8 @@ public class ProjectDetailController {
     private TaskService taskService;
 
     @GetMapping
-    public String getProjectDetail(@PathVariable("projectId") long projectId, Model model) {
+    public String getProjectDetail(@PathVariable("projectId") long projectId, Model model, HttpServletRequest request) {
         Project project = projectService.getProjectByProjectId(projectId);
-//        model.addAttribute("project", project.getProjectId());
-//        model.addAttribute("projectId", projectId);
         model.addAttribute("projectTitle", project.getProjectName());
         model.addAttribute("projectState", project.getProjectState());
 
