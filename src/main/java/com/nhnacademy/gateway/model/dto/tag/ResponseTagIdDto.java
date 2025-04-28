11bf131abbdf085e6c7ad374476ad9fc7bcdf49e
김재혁 <rlawog@nate.com>
2025-04-29 00:13:23 +0900

    feat. 태그 리스트 수정중

--- src/main/java/com/nhnacademy/gateway/model/dto/tag/ResponseTagIdDto.java
+++ src/main/java/com/nhnacademy/gateway/model/dto/tag/ResponseTagIdDto.java
@@ -1,5 +1,15 @@
 package com.nhnacademy.gateway.model.dto.tag;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseTagIdDto {
     private long tagId;
+    private String tagName;
 }
--- src/main/java/com/nhnacademy/gateway/model/dto/tag/ResponseTagListByTaskDto.java
+++ src/main/java/com/nhnacademy/gateway/model/dto/tag/ResponseTagListByTaskDto.java
@@ -1,7 +1,16 @@
 package com.nhnacademy.gateway.model.dto.tag;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseTagListByTaskDto {
     List<ResponseTagIdDto> tagList;
 }
